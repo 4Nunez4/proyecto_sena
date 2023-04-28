d0e6e2fbbde79fe22858ecf18badf25630e82820
4Nunez4 <jorgeenrrique123456789@gmail.com>
2023-04-28 16:12:51 -0500

    factura

--- jc/formulario.js
+++ jc/formulario.js
@@ -1,6 +1,7 @@
 function formulario(){
     let producto = document.getElementById("nom-producto").value
     document.getElementById("res-producto").innerText = producto
+    
     let produccion = document.getElementById("nom-produccion").value
     document.getElementById("res-produccion").innerText = produccion
     console.log(producto, produccion)
