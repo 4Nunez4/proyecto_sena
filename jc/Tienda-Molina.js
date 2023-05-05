862cab635a144fd5731d89708fd89692323a599a
4Nunez4 <jorgeenrrique123456789@gmail.com>
2023-05-04 20:53:52 -0500

    tienda

--- jc/Tienda-Molina.js
+++ jc/Tienda-Molina.js
@@ -28,6 +28,14 @@ function precio2(){
 function calcularTotal(precio1, precio2) {
     document.getElementById('totalAbsoluto').innerText = parseInt(precio1) + parseInt(precio2)
 }
+function limpiarCantidad() {
+    document.getElementById('cantidad1').value = " ";
+    document.getElementById('cantidad2').value = "";
+    document.getElementById('total1').innerText = "";
+    document.getElementById('total2').innerText = "";
+    document.getElementById('totalAbsoluto').innerText = "";
+    document.getElementById('factura').innerText ="";
+  }
 
 function getFactura() {
     var selectProducto1 = document.getElementById('productos')
@@ -43,8 +51,18 @@ function getFactura() {
     var precio2 = document.getElementById('precio2').value
 
     var factura = document.getElementById('factura')
-    factura.innerHTML = `<li><p>${nomProducto1}</p><p>${cantidad1}</p><p>${precio1}</p></li>`
-    factura.innerHTML += `<li><p>${nomProducto2}</p><p>${cantidad2}</p><p>${precio2}</p></li>`
+    factura.innerHTML = `<li><p>${nomProducto1}</p><p>Cantidad: ${ cantidad1}</p><p>Precio productos: ${ cantidad1 * precio1}</p></li>`
+    factura.innerHTML += `<li><p>${nomProducto2}</p><p>cantidad: ${ cantidad2}</p><p>Precio productos: ${ cantidad2 * precio2}</p></li>`
     factura.innerHTML += `<li><p>total</p><p>${(cantidad1 * precio1) + (cantidad2 * precio2)}</p></li>`
+    var cantidad1 = document.getElementById('cantidad1').value
+    var precio1 = document.getElementById('precio1').value
+    var total1 = precio1 * cantidad1
+    document.getElementById('total1').innerText = total1
+
+    var cantidad2 = document.getElementById('cantidad2').value
+    var precio2 = document.getElementById('precio2').value
+    var total2 = cantidad2 * precio2
+    document.getElementById('total2').innerText = total2
+    calcularTotal(total1, total2)
 }
 
