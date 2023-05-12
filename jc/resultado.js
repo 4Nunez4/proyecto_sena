6f45f20cb2a588a1dfeea55f5647b38c4d1545b0
4Nunez4 <jorgeenrrique123456789@gmail.com>
2023-05-12 16:34:49 -0500

    formulario

--- jc/resultado.js
+++ jc/resultado.js
@@ -1,16 +1,3 @@
 window.onload = function(){
     cargarDatos()
 }
-const N = document.querySelector("#datoNombre");
-const P = document.querySelector("#datoApellido");
-const F = document.querySelector("#datoFecha")
-function cargarDatos(){
-    let nombre, apellido ,fecha
-    nombre = localStorage.getItem("Nombre")
-    apellido = localStorage.getItem("Apellido")
-    fecha = localStorage.getItem("Fecha")
-    N.innerHTML= nombre
-    P.innerHTML= apellido
-    F.innerHTML= fecha
-}
-cargarDatos()
