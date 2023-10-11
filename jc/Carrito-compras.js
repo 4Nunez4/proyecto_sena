1ef1f180668a50a71cf3dc1962ff9a0424667f5f
4Nunez4 <jorgeenrrique123456789@gmail.com>
2023-10-11 15:45:46 -0500

    carrito index

--- jc/Carrito-compras.js
+++ jc/Carrito-compras.js
@@ -1,20 +1,50 @@
 let boxMinus = document.getElementById("boxMinus"),
     boxCant = document.getElementById("boxCant"),
-    boxPlus = document.getElementById("boxPlus");
+    boxPlus = document.getElementById("boxPlus"),
+    numeroProductos = document.getElementById("numeroPoductos"),
+    ValorProducto = 50000 ,
+    auxValorProducto = new Intl.NumberFormat("de-DE").format(ValorProducto);
+    document.getElementById("precioUnidad").innerHTML = auxValorProducto;
 
 
+    
     boxPlus.addEventListener("click", function(){
         let PlusCant = boxCantidad.innerHTML;
         if(PlusCant < 10){
             PlusCant++;
-            boxCantidad.innerText=PlusCant;
+            boxCantidad.innerText=PlusCant
+            determinarSingularPlural(PlusCant)
+            //calcular precio total
+    let SubTotal = ValorProducto*PlusCant,
+    auxSubtotal = new Intl.NumberFormat("de-DE").format(SubTotal);
+    document.getElementById("Subtotal").innerHTML = auxSubtotal;
+
+
         }
         
     })
+
+
     boxMinus.addEventListener("click", function(){
         let MinusCant = boxCantidad.innerHTML;
         if(MinusCant > 0){
             MinusCant--;
-            boxCantidad.innerText=MinusCant;
+            boxCantidad.innerText=MinusCant 
+            determinarSingularPlural(MinusCant)
+    
+            // calcular precio total
+            let SubTotal = ValorProducto*MinusCant ,
+            auxSubtotal = new Intl.NumberFormat("de-DE").format(SubTotal);
+            document.getElementById("Subtotal").innerHTML = auxSubtotal;
+
+
         }
     })
+    function determinarSingularPlural(PlusCant){
+        if (PlusCant == 1){
+            numeroProductos.innerHTML ="( " + PlusCant + " producto)"
+        }
+        else if ((PlusCant == 0 || PlusCant > 1)) {
+            numeroProductos.innerHTML = "( " + PlusCant + " productos)"
+        }
+    }
