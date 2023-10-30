1f6a7a46b21420fe374292b8d0ccb08145ae4a42
4Nunez4 <jorgeenrrique123456789@gmail.com>
2023-10-30 03:03:23 -0500

    amazon

---
+++ jc/amazon.js
@@ -0,0 +1,43 @@
+let btnClose = document.getElementById("btnClose"),
+mainContent = document.getElementById("mainContent"),
+imgModal = document.getElementById("imgModal");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Event Listener for the close button
+btnClose.addEventListener('click', function(){
+    mainContent.style.display = "none";
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
