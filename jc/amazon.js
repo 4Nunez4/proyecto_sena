d29cca1355bac9ab0548c70dd28ae3408672404f
4Nunez4 <jorgeenrrique123456789@gmail.com>
2023-11-29 14:01:01 -0500

    cambio de imagenes

--- jc/amazon.js
+++ jc/amazon.js
@@ -4,6 +4,7 @@ mainContent = document.getElementById("mainContent"),
 // Obtén todas las imágenes dentro del contenedor box-modal-img
 imagenes = document.querySelectorAll('.box-modal-img img'),
 
+
 // Obtén el contenedor imgModal
 imgModal = document.getElementById('imgModal');
 
@@ -19,6 +20,24 @@ imagenes.forEach((imagen) => {
 btnClose.addEventListener('click', function(){
     mainContent.style.display = "none";
     });
+
+    // Seleccionar todos los elementos con la clase box-imagen-muestra
+let miniaturas = document.querySelectorAll(".box-imagen-muestra");
+
+// Seleccionar el elemento con el id imgBig
+let imagenGrande = document.getElementById("imgBig");
+
+// Recorrer el array de miniaturas con un bucle for
+for (let i = 0; i < miniaturas.length; i++) {
+  // Añadir un evento de click a cada miniatura
+  miniaturas[i].addEventListener("click", function() {
+    // Obtener el atributo src de la imagen dentro de la miniatura
+    let srcMiniatura = miniaturas[i].querySelector("img").getAttribute("src");
+    // Cambiar el atributo src de la imagen grande por el de la miniatura
+    imagenGrande.setAttribute("src", srcMiniatura);
+  });
+}
+
 
 //////////////////////////////////////////////////////////////////////
 let star1 = document.getElementById("star1"),
